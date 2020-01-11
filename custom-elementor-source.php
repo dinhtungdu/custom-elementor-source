885fbbd8add4c2a7b55ef49164396110a5276aa2
Tung Du <dinhtungdu@gmail.com>
2020-01-11 16:32:32 +0700

    Use closure binding to support Elementor 2.7+

--- custom-elementor-source.php
+++ custom-elementor-source.php
@@ -19,6 +19,12 @@
  */
 add_action( 'elementor/init', function() {
 	include 'includes/source.php';
-	\Elementor\Plugin::instance()->templates_manager->unregister_source( 'remote' );
+	
+	// Unregister source with closure binding, thank Steve.
+	$unregister_source = function($id) {
+		unset( $this->_registered_sources[ $id ] );
+	};
+
+	$unregister_source->call( \Elementor\Plugin::instance()->templates_manager, 'remote');
 	\Elementor\Plugin::instance()->templates_manager->register_source( 'Elementor\TemplateLibrary\Source_Custom' );
 }, 15 );
